60770db4101cd71b285ff30f334d1ded9279e09f
Suchitra Naik <susanaik@gmail.com>
2018-10-23 18:06:53 +0000

    automatically backed up by learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class MovieShowcase extends Component{
+export default class MovieShowcase extends React.Component{
   render(){
     return(
       <div>
